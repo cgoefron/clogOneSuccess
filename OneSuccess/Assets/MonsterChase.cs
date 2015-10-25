8337e23278053973c16eaaf17b0d9c8bb63df6ba
Christina Goefron <cagoefron@gmail.com>
2015-10-24 22:23:59 -0400

    Added Monster Chase and Throw scripts.

---
+++ OneSuccess/Assets/MonsterChase.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterChase : MonoBehaviour {
+
+	Transform playerPosition; 
+	public float speed;
+	
+	// Use this for initialization
+	void Start () {
+		playerPosition = GameObject.Find ("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (playerPosition.position);
+		transform.position += speed * Time.deltaTime * transform.forward;
+	}
+
+	void onTriggerEnter(Collider other){
+		if (other.tag == "Player") {
+			Application.LoadLevel("endScene");; //make a death scene
+		}
+	}
+}
---
+++ OneSuccess/Assets/sceneChanger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sceneChanger : MonoBehaviour { //Use this script if we make a start screen
+
+
+	void Start () {
+		if (Input.GetKeyDown ("x")) {
+			Application.LoadLevel("ChristinaScene"); //change to whatever game scene when ready
+		}
+}
+}
