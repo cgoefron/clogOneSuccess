6d5d9905488f6d8e1376b716bbef38cb45ccb9a8
Charlotte Alvelita Grillet <CharlotteGrillet@Charlotte-Grillet.local>
2015-10-27 17:52:14 -0400

    Sky Transitions

---
+++ OneSuccess/Assets/changesky.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changesky : MonoBehaviour {
+
+	public GameObject theSun;
+	public bool isNight = false;
+	public float num = 10000;
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!isNight) {
+			theSun.GetComponent<Light>().range = 1;
+	}
+		else
+		{
+			theSun.GetComponent<Light>().range += 1;
+			if(theSun.GetComponent<Light>().range >= num)
+			{
+				theSun.GetComponent<Light>().enabled = true;
+				isNight = false; 
+			}
+
+		}
+	
+	}
+}
